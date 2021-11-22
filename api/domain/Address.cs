84d34a58acf2c45a8c6635ff8012fce2d2b1b60e
JortWillemsen <jort@maurice-nienke.nl>
2021-11-22 09:54:37 +0100

    Add address domain class and import database dependency

---
+++ api/domain/Address.cs
@@ -0,0 +1,20 @@
+﻿namespace SBAddressAPI.domain
+{
+    public class Address
+    {
+        public Address(string street, int homeNumber, string postalCode, string city, string country)
+        {
+            Street = street;
+            HomeNumber = homeNumber;
+            PostalCode = postalCode;
+            City = city;
+            Country = country;
+        }
+
+        private string Street { get; set; }
+        private int HomeNumber { get; set; }
+        private string PostalCode { get; set; }
+        private string City { get; set; }
+        private string Country { get; set; }
+    }
+}
